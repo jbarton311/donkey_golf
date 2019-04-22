e6516bf6478e21c3ea6a58a5a9d5dfc18ecf920b
Jeff B <vtbandofhorses@gmail.com>
2019-04-21 22:08:23 -0400

    Allowing tourney_info to load to database

--- donkey_golf/data_things/tournament_info.py
+++ donkey_golf/data_things/tournament_info.py
@@ -37,18 +37,24 @@ class TournamentInfo(BaseClass):
         tournament_info = espn.find('div', class_='Leaderboard__Header')
         tourney_data = {}
         course_data = tournament_info.find('div', class_= 'Leaderboard__Course__Location__Detail n8 clr-gray-04').text
-        tourney_data['tourney_name'] = tournament_info.find('h1', class_='headline__h1 Leaderboard__Event__Title').text
-        tourney_data['tourney_dates'] = tournament_info.find('span', class_='Leaderboard__Event__Date n7').text
-        tourney_data['course_name'] = course_name = tournament_info.find('div', class_= 'Leaderboard__Course__Location n8 clr-gray-04').text
-        tourney_data['course_par'] = course_data[3:5]
-        tourney_data['course_yardage'] = course_data[-4:]
+        tourney_data['tourney_name'] = [tournament_info.find('h1', class_='headline__h1 Leaderboard__Event__Title').text]
+        tourney_data['tourney_dates'] = [tournament_info.find('span', class_='Leaderboard__Event__Date n7').text]
+        tourney_data['course_name'] = [tournament_info.find('div', class_= 'Leaderboard__Course__Location n8 clr-gray-04').text]
+        tourney_data['course_par'] = [course_data[3:5]]
+        tourney_data['course_yardage'] = [course_data[-4:]]
         tournament_winner_data = tournament_info.find('div', class_= 'n7 clr-gray-04')
         winner = []
         for node in tournament_winner_data.find_all('span', class_= 'clr-gray-05 mr2 label'):
             winner.append(node.next_sibling)
-        tourney_data['tourney_purse'] = winner[0]
-        tourney_data['tourney_defending_champ'] = winner[1]
+        tourney_data['tourney_purse'] = [winner[0]]
+        tourney_data['tourney_defending_champ'] = [winner[1]]
         self.data_dict = tourney_data
+
+    def convert_to_dataframe(self):
+        self.data = pd.DataFrame(self.data_dict)
+        self.data['tourney_id'] = self.tourney_id
 
     def run(self):
         self.pull_current_tourney_data()
+        self.convert_to_dataframe()
+        self.load_table_to_db(self.data, 'tourney_info')
